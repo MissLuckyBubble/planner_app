128404af441417f2154df3ccd256a1d897523b1b
MissLuckyBubble <qvkatadlg9@gmail.com>
2023-06-03 16:07:15 +0300

    better fetching appointment

--- app/screens/MyAppointmentsScreen.js
+++ app/screens/MyAppointmentsScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState, } from 'react';
-import { View, ScrollView, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, ScrollView, StyleSheet, Text, TouchableOpacity, Alert, ToastAndroid } from 'react-native';
 import AppointmentComponent from '../components/AppointmentComponent';
 import axios from 'axios';
 import { AuthContext } from '../context/AuthContext';
@@ -12,6 +12,7 @@ import { faArrowLeft, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const MyAppointmentsScreen = ({ navigation }) => {
     const isFocused = useIsFocused();
+    const [loading, setLoading] = useState(false);
 
     const openDrawer = () => {
         navigation.openDrawer();
@@ -29,25 +30,38 @@ const MyAppointmentsScreen = ({ navigation }) => {
     };
 
     const fetchAppointments = async () => {
+
+        if (loading) {
+            console.log('fetchAppointments - stoped');
+            return; // Prevent multiple simultaneous calls
+        }
+
+        setLoading(true); // Start loading
+
+        console.log('fetchAppointments');
         const currentDate = new Date();
         const year = currentDate.getFullYear();
         const month = String(currentDate.getMonth() + 1).padStart(2, "0");
         const day = String(currentDate.getDate()).padStart(2, "0");
         const status = 'Запазен';
         const formattedDate = `${year}/${month}/${day}`;
+        const cacheBuster = new Date().getTime(); // Generate a unique value for each request
+        console.log('formattedDate:', formattedDate);
         try {
             const response = await axios.get(
-                `${BASE_URL}/customer/appointments/getAll?date_after=${formattedDate}&status=${status}`,
+                `${BASE_URL}/customer/appointments/getAll?date_after=${formattedDate}&status=${status}&cacheBuster=${cacheBuster}`,
                 config
             );
-            const result = response.data.data;
-            if (result) {
-                setAppointments(result);
-            } else {
-                setAppointments([]);
+            console.log(response);
+            console.log('Response status:', response.status);
+            if (response.data.data) {
+                console.log(response.data.data);
+                setAppointments(response.data.data);
             }
         } catch (error) {
             console.log(error);
+        }finally {
+            setLoading(false); // Stop loading
         }
     };
 
@@ -59,15 +73,31 @@ const MyAppointmentsScreen = ({ navigation }) => {
         navigation.goBack();
     };
 
-    const handleCancel = (id) => {
-        const url = `${BASE_URL}/customer/appointments/cancel/${id}`;
-        axios.patch(url, '', config).then((response) => {
-            console.log(response.data);
-            fetchAppointments();
-        }).catch((error) => {
-            console.error(error)
-        });
+    const handleCancel = async (id) => {
+        Alert.alert(
+            'Отказване на час',
+            'Сигурни ли сте, че искате да откажете този час, това ще причини загуби за бизнеса?',
+            [
+                {
+                    text: 'не отказвай',
+                    style: 'cancel',
+                },
+                {
+                    text: 'Да, откажи',
+                    onPress: async () => {
+                        const url = `${BASE_URL}/customer/appointments/cancel/${id}`;
+                        axios.patch(url, '', config).then((response) => {
+                            ToastAndroid.show(`Успешно отказан час`, ToastAndroid.SHORT);
+                            fetchAppointments();
+                        }).catch((error) => {
+                            console.error(error)
+                        });
+                    },
+                },
+            ],
+        );
     }
+
 
     return (
         <ScrollView style={styles.container}>
@@ -123,7 +153,7 @@ const styles = StyleSheet.create({
     scrollView: {
         flex: 1,
         marginTop: 10,
-        marginHorizontal:25
+        marginHorizontal: 25
     },
 });
 
