4f5c78d0aad6e9133abcc169c49f243e427a629e
MissLuckyBubble <qvkatadlg9@gmail.com>
2023-06-04 18:05:31 +0300

    changes to work with group appointments

--- app/components/ModalServiceFormComponent.js
+++ app/components/ModalServiceFormComponent.js
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { ImageBackground, StyleSheet, View, Modal, TextInput, TouchableOpacity, Text } from 'react-native';
 import { Colors } from '../assets/Colors';
+import CheckBox from '@react-native-community/checkbox';
+import { TextInputMask } from 'react-native-masked-text';
 
 const ModalServiceFormComponent = ({ visible, onCancel, onSave, initialData }) => {
     const [title, setTitle] = useState('');
-    const [duration_minutes, setDurationMinutes] = useState('');
+    const [duration, setDurationMinutes] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
+    const [is_group, setIsGroup] = useState(false);
+    const [date, setDate] = useState('');
+    const [hour, setHour] = useState('');
+    const [maxCapacity, setMaxCapacity] = useState('');
 
     useEffect(() => {
         if (initialData) {
             setTitle(initialData.title);
-            setDurationMinutes(initialData.duration_minutes);
+            setDurationMinutes(initialData.duration);
             setDescription(initialData.description);
             setPrice(initialData.price);
         }
@@ -25,9 +31,12 @@ const ModalServiceFormComponent = ({ visible, onCancel, onSave, initialData }) =
     const handleSave = () => {
         onSave({
             title,
-            duration_minutes,
+            duration,
             description,
             price,
+            maxCapacity,
+            date,
+            hour
         });
         clearForm();
     };
@@ -38,12 +47,36 @@ const ModalServiceFormComponent = ({ visible, onCancel, onSave, initialData }) =
         setDurationMinutes('');
         setDescription('');
         setPrice('');
+        setIsGroup(false);
+        setMaxCapacity('');
+        setDate('');
+        setHour('');
     };
 
     return (
         <Modal visible={visible} animationType="slide">
-            <ImageBackground  style={styles.container} source={require("../assets/bg-simple.jpg")}>
+            <ImageBackground style={styles.container} source={require("../assets/bg-simple.jpg")}>
                 <Text style={styles.title}>Добавяне на услуга</Text>
+                <View style={styles.row}>
+                    <Text style={styles.text}>Групов час:</Text>
+                    <CheckBox
+                        value={is_group}
+                        onValueChange={(newValue) => setIsGroup(newValue)}
+                        style={{ marginBottom: 10 }}
+                    />
+                    {is_group &&
+                        <View style={styles.row}>
+                            <Text style={styles.text}> Брой Хора</Text>
+                            <TextInput
+                                style={styles.input}
+                                value={maxCapacity}
+                                min={0}
+                                onChangeText={setMaxCapacity}
+                                placeholder="2"
+                                keyboardType="numeric"
+                            />
+                        </View>}
+                </View>
                 <TextInput
                     style={styles.input}
                     value={title}
@@ -52,13 +85,16 @@ const ModalServiceFormComponent = ({ visible, onCancel, onSave, initialData }) =
                 />
                 <TextInput
                     style={styles.input}
-                    value={duration_minutes}
+                    value={duration}
                     onChangeText={setDurationMinutes}
                     placeholder="Времетраене"
                     keyboardType="numeric"
                 />
                 <TextInput
-                    style={styles.input}
+                    style={[styles.input, { height: 100 }]}
+                    maxLength={500}
+                    multiline
+                    numberOfLines={2}
                     value={description}
                     onChangeText={setDescription}
                     placeholder="Описание"
@@ -70,12 +106,35 @@ const ModalServiceFormComponent = ({ visible, onCancel, onSave, initialData }) =
                     placeholder="Цена"
                     keyboardType="numeric"
                 />
+                {is_group &&
+                    <View>
+                        <View style={styles.row}>
+                            <Text style={styles.text}>Дата</Text>
+                            <TextInputMask
+                                type={'datetime'}
+                                options={{ format: 'YYYY-MM-DD' }}
+                                placeholder={'yyyy-mm-dd'}
+                                value={date}
+                                onChangeText={setDate}
+                                style={styles.input}
+                            />
+                            <Text style={styles.text}>Час</Text>
+                            <TextInputMask
+                                type={'datetime'}
+                                options={{ format: 'HH:mm' }}
+                                placeholder={'00:00'}
+                                value={hour}
+                                onChangeText={setHour}
+                                style={styles.input}
+                            />
+                        </View>
+                    </View>}
 
-                <TouchableOpacity style={styles.button} onPress={handleCancel}>
-                    <Text style={styles.buttonText}>Отказ</Text>
-                </TouchableOpacity>
                 <TouchableOpacity style={[styles.button, { backgroundColor: Colors.dark }]} onPress={handleSave}>
                     <Text style={[styles.buttonText, { color: Colors.white }]}>Запази</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button} onPress={handleCancel}>
+                    <Text style={styles.buttonText}>Отказ</Text>
                 </TouchableOpacity>
             </ImageBackground>
         </Modal>
@@ -119,6 +178,16 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: Colors.dark
     },
+    row: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 5
+    },
+    text:{
+        marginHorizontal:10,
+        marginLeft:20,
+        marginBottom:10
+    }
 });
 
 
